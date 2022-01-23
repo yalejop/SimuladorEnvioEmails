3da17489a5e0730617fadb46a981e253d3bfdd09
yalejop <alejo.9211@hotmail.com>
2022-01-23 08:10:38 -0500

    cleaning error message´s in the inputs

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 //variables
 const btnEnviar = document.querySelector('#enviar');
+const formulario = document.querySelector('#enviar-mail');
 
 //variables campos
 const email = document.querySelector('#email');
@@ -28,6 +29,20 @@ function validarFormulario(e) {
         console.log('tiene algo')
     } else {
         e.target.classList.add('border', 'border-red-500')
+
+        mostrarError()
     }
 }
 
+function mostrarError() {
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = 'Todos los campos son obligatorios'; 
+    mensajeError.classList.add('border', 'border-red-500', 'background-red-100', 'text-red-500', 'p-3', 'mt-5', 'text-center', 'error');
+
+    const errores = document.querySelectorAll('.error');
+    if (errores.length === 0) {
+        formulario.appendChild(mensajeError);
+    }
+
+    
+}
