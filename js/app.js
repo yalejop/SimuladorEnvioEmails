888c92d77b94f2d4aa2223507ed33af2832f290c
yalejop <alejo.9211@hotmail.com>
2022-01-23 09:51:37 -0500

    change input´s color when validation is passed

--- js/app.js
+++ js/app.js
@@ -26,17 +26,39 @@ function IniciarApp() {
 
 function validarFormulario(e) {
     if (e.target.value.length > 0) {
-        console.log('tiene algo')
+
+        //eliminar los errores...
+
+        const error = document.querySelector('p.error');
+        if( error ){
+            error.remove();
+         }
+        
+        e.target.classList.remove('border', 'border-red-500')
+        e.target.classList.add('border', 'border-green-500')
     } else {
+        e.target.classList.remove('border', 'border-green-500')
         e.target.classList.add('border', 'border-red-500')
 
         mostrarError('Todos los campos son obligatorios');
     }
 
     if (e.target.type === 'email') {
-        const resultado = e.target.value.indexOf('@');
-        if(resultado < 0) {
-            mostrarError('Debes colocar un email válido')
+        const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if( er.test( e.target.value ) ) {
+            const error = document.querySelector('p.error');
+            if( error ){
+                error.remove();
+            }
+            
+            e.target.classList.remove('border', 'border-red-500')
+            e.target.classList.add('border', 'border-green-500')
+        } else {
+            e.target.classList.remove('border', 'border-green-500')
+            e.target.classList.add('border', 'border-red-500')
+
+            mostrarError('Email No Válido');
         }
         
     }
