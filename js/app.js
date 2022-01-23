b322172cf47515c251487460faacf7f8283d0258
yalejop <alejo.9211@hotmail.com>
2022-01-22 22:58:27 -0500

    validate inputs

--- js/app.js
+++ js/app.js
@@ -1,19 +1,33 @@
 //variables
 const btnEnviar = document.querySelector('#enviar');
 
-
+//variables campos
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje');
 
 eventListeners();
 function eventListeners(){
+    //cuando se carga la app
     document.addEventListener('DOMContentLoaded', IniciarApp);
+
+    //campos del formulario
+    email.addEventListener('blur', validarFormulario);
+    asunto.addEventListener('blur', validarFormulario);
+    mensaje.addEventListener('blur', validarFormulario);
 }
 
 //funciones
 function IniciarApp() {
     btnEnviar.disabled = true;
     btnEnviar.classList.add('cursor-not-allowed', 'opacity-50')
-
 }
 
-
+function validarFormulario(e) {
+    if (e.target.value.length > 0) {
+        console.log('tiene algo')
+    } else {
+        e.target.classList.add('border', 'border-red-500')
+    }
+}
 
