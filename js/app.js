a22870432807b542682781a7d92ad5d2d93f44d7
yalejop <alejo.9211@hotmail.com>
2022-01-23 08:38:11 -0500

    validate email input

--- js/app.js
+++ js/app.js
@@ -30,13 +30,21 @@ function validarFormulario(e) {
     } else {
         e.target.classList.add('border', 'border-red-500')
 
-        mostrarError()
+        mostrarError('Todos los campos son obligatorios');
+    }
+
+    if (e.target.type === 'email') {
+        const resultado = e.target.value.indexOf('@');
+        if(resultado < 0) {
+            mostrarError('Debes colocar un email válido')
+        }
+        
     }
 }
 
-function mostrarError() {
+function mostrarError(mensaje) {
     const mensajeError = document.createElement('p');
-    mensajeError.textContent = 'Todos los campos son obligatorios'; 
+    mensajeError.textContent = mensaje; 
     mensajeError.classList.add('border', 'border-red-500', 'background-red-100', 'text-red-500', 'p-3', 'mt-5', 'text-center', 'error');
 
     const errores = document.querySelectorAll('.error');
